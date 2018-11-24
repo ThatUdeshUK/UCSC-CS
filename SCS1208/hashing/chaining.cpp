3b6bef8bf56aa8c7d39c6498cb579e55c4f6a939
Udesh Kumarasinghe <udesh.upalanka@live.com>
2018-11-25 05:15:03 +0530

    Add hashing with chaining example

---
+++ SCS1208/hashing/chaining.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+class HashMap
+{
+  private:
+    int m;
+    vector<pair<int, T>> *table;
+
+    int hash_func(int key) {
+        return key % m;
+    }
+
+  public:
+    static const int NULL_KEY = -1998;
+
+    HashMap(int m){
+        this->m = m;
+        this->table = new vector<pair<int, T>>[m];
+    };
+
+    ~HashMap(){
+        for(size_t i = 0; i < m; i++)
+        {
+            vector<pair<int, T>>().swap(this->table[i]);
+        }
+        
+        delete [] table;
+    }
+
+    void insert(int key, T value) {
+        this->table[hash_func(key)].push_back(make_pair(key, value));
+    }
+
+    T search(int key) {
+        vector<pair<int, T>> cell = this->table[hash_func(key)];
+        
+        for(size_t i = 0; i < cell.size(); i++)
+        {
+            if (cell[i].first == key) {
+                return cell[i].second;
+            }
+        }
+        return NULL_KEY;
+    }
+};
+
+int main() {
+    HashMap<int> hashMap(7);
+    hashMap.insert(20, 100);
+    hashMap.insert(9, 200);
+    hashMap.insert(17, 300);
+    hashMap.insert(30, 400);
+    cout << hashMap.search(30) << endl;
+    cout << hashMap.search(17) << endl;
+    cout << hashMap.search(9) << endl;
+    if (hashMap.search(10) == HashMap<int>::NULL_KEY) {
+        cout << "Not found" << endl;
+    }
+    return 0;
+}
